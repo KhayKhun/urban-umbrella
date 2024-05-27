de9514451a556b1d13cefcb7172aa8987a368555
KhayKhun <saikhaykhunmong@gmail.com>
2024-05-27 18:02:40 +0630

    Add comments

--- projects/notepad.py
+++ projects/notepad.py
@@ -2,15 +2,18 @@ import tkinter as tk
 import datetime
 from tkinter import filedialog
 import os
+
+# root config
 root = tk.Tk()
 root.iconbitmap("../images/logo.ico")
-date = datetime.datetime.now()
-root.title("Happy " + date.strftime("%A")+ "!!")
 
-menu_bar= tk.Menu(root)
+# declare
+date = datetime.datetime.now()
+root.title("Happy " + date.strftime("%A")+ "!!") # happy monday
 text_box = tk.Text(root,height=30, padx=20, pady=20)
 clock = tk.Label(text= date.strftime("%d"+"/"+"%m"+"/"+"%y"))
 
+# functions
 def save():  
     path = filedialog.asksaveasfilename(
         defaultextension=".txt",
@@ -44,12 +47,13 @@ def open_file():
         text_box.delete(1.0, tk.END)
         for line in lines:
             text_box.insert(tk.END, line)
-            text_box.insert(tk.END, line)
 
+# menu bar
+menu_bar= tk.Menu(root)
 menu_bar.add_command(label="save",command=save)
 menu_bar.add_command(label="open",command=open_file)
 
-
+# packing section
 root.config(menu=menu_bar)
 text_box.pack()
 clock.pack()
