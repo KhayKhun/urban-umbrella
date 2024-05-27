9b6a55aadc21fbe0f0e77510a708ce2bb15e5f67
KhayKhun <saikhaykhunmong@gmail.com>
2024-05-27 13:07:00 +0630

    fd

--- helpers/filedialog.py
+++ helpers/filedialog.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
 def selectfolder():
-    path = filedialog.askdirectory()
-    print(path)    
+    path = filedialog.askdirectory() # folder choose
+
+    path = filedialog.askopenfilename() # open a file
+   
+    path = filedialog.asksaveasfilename( # save a file
+        filetypes=[("Text files","*.txt"),("All files","*.*")] # accept file types
+    )
+
+    print(path)      
 
 root = tk.Tk()
 b = tk.Button(root, text="click", command=selectfolder)
