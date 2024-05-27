6fa6fb869f62f5aff9de5ea3e8e12ee4acb06194
KhayKhun <saikhaykhunmong@gmail.com>
2024-05-27 13:19:37 +0630

    fd

--- projects/notepad.py
+++ projects/notepad.py
@@ -4,28 +4,17 @@ import datetime
 root = tk.Tk()
 
 t = tk.Text(root,height=30, padx=20, pady=20)
+def save():
+    print(t.get())
 date = datetime.datetime.now()
 title = tk.Label(text="Happy"+" " +date.strftime("%A")+ "!!")
-
+b = tk.Button(root, text="save", command=save)
 t1 = tk.Label(text= date.strftime("%d"+"/"+"%m"+"/"+"%y"))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 title.pack()
+b.pack()
 t.pack()
 
 t1.pack()
