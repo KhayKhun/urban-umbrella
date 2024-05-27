4b95ee8c6b28b623480181ede6f0bd81935b2424
NayDweNaing <ndnaing2004@gmail.com>
2024-05-27 13:07:35 +0630

    Sample Design

--- projects/notepad.py
+++ projects/notepad.py
@@ -1,9 +1,33 @@
 import tkinter as tk
-
+import datetime
 
 root = tk.Tk()
 
-t = tk.Text(root,height=30)
+t = tk.Text(root,height=30, padx=20, pady=20)
+date = datetime.datetime.now()
+title = tk.Label(text="Happy"+" " +date.strftime("%A")+ "!!")
+
+t1 = tk.Label(text= date.strftime("%d"+"/"+"%m"+"/"+"%y"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+title.pack()
 t.pack()
+
+t1.pack()
 
 root.mainloop()
