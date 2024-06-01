7c23be484707d6c32d7f3c19f994ef1a943042e8
KhayKhun <saikhaykhunmong@gmail.com>
2024-06-01 22:46:01 +0630

    barnyaar

---
+++ helpers/barnyar.py
@@ -0,0 +1,25 @@
+people = ["ku","ju","paing","pu","poo","aung","phyo"]
+
+for i in range(0,len(people)): # whole number
+    print(people[i])
+
+print("---------------------")
+
+for p in people:
+    print(p)
+
+
+def sumNums(x,y):
+    return x + y
+
+result = sumNums(5,6)
+print(result)
+
+range(0,5,6) # built in function, that returns a list
+
+x = people[:5] # not a funtion
+
+# funtions
+print(x)
+int('3')
+str(6)
